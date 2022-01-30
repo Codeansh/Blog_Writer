f2ab40bfdc67c8441f6fa174789fa8d59df4777f
Shivansh <ansh02shiv@gmail.com>
2022-01-30 13:35:02 +0530

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from app import app_
+from flask import render_template
+
+@app_.route('/')
+@app_.route('/index')
+def index():
+    user = {'username':'roy'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }]
+    return render_template('index.html',user=user,posts=posts)
