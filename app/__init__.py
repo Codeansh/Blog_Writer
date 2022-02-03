d66f584ba310a4d14b084ffa46b2a6a6c5cd3e70
Shivansh <ansh02shiv@gmail.com>
2022-02-03 18:13:33 +0530

    transfer data

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
-app_ = Flask(__name__)
+app = Flask(__name__)
+app.config.from_object(Config)
+login =LoginManager(app)
+login.login_view = 'login'
+# login.login_message = "User needs to be logged in to view this page"
 
-from app import routes
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
---
+++ app/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo, Length
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class Edit_Profile_Form(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password', validators=[EqualTo('password'), DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already Exists.')
+
+    def validate_email(self, email):
+        mail = User.query.filter_by(email=email.data).first()
+        if mail is not None:
+            raise ValidationError("Email is already registered")
