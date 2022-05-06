f7f31cabff75a1bba6cf2a255048cb0d0f3c4c7c
Codeansh <ansh02shiv@gmail.com>
2022-05-06 23:15:14 +0530

    Application before registering blueprints

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.8-slim-buster
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip3 install -r requirements.txt
+
+COPY . .
+
+ENV FLASK_APP main.py
+ENV FLASK_ENV development
+
+EXPOSE 5001
+
+CMD ["python3","-m","flask", "run", "--host=0.0.0.0"]
--- config.py
+++ config.py
@@ -5,3 +5,11 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['noreply.12345c@gmail.com', 'ansh02shiv@gmail.com']
+    POSTS_PER_PAGE = 3
+    LANGUAGES = ['en', 'es']
